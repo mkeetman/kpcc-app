824784381775fcc1fd2709a204b5d14549223cc0
Michael Keetman <michael@home.keetman.org>
2015-07-16 21:32:47 +1000

    tidy up

--- rides/urls.py
+++ rides/urls.py
@@ -7,9 +7,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9])/cancel/$', views.CancelRideDetail.as_view(), name='cancel_detail'),
-    url(r'^(?P<pk>[0-9])/cancel/submit/$', views.cancel_ride, name='cancel_ride'),
-    url(r'^(?P<pk>[0-9])/cancel/uncancel/$', views.uncancel_ride, name='un-cancel_ride'),
+    url(r'^(?P<pk>[0-9]+)/cancel/$', views.CancelRideDetail.as_view(), name='cancel_detail'),
+    url(r'^(?P<pk>[0-9]+)/cancel/submit/$', views.cancel_ride, name='cancel_ride'),
+    url(r'^(?P<pk>[0-9]+)/cancel/uncancel/$', views.uncancel_ride, name='un-cancel_ride'),
     url(r'^nextride/status/$', views.next_ride, name='next_ride_status'),
     #url(r'^login/$', auth_views.login),
     #url(r'^(?P<pk>[0-9])/status/$', views.RideDetail.as_view(), name='status'),
